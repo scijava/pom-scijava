9f9f7603da57dee4e43205785b6dd97abcca174b
Mark Hiner <hinerm@gmail.com>
2021-07-09 09:35:07 -0500

    CI: remove Travis CI configuration

--- .travis/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh $encrypted_e7761bcf8400_key $encrypted_e7761bcf8400_iv
-#sh tests/run.sh
