4a95ff69be506ac2c2a2fbe303ea84e9074d204b
hinerm <hinerm@gmail.com>
2022-09-23 14:30:36 -0500

    Run the melting-pot as part of CI

--- .github/build.sh
+++ .github/build.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-build.sh
 sh ci-build.sh
+
+# Record the last time of cache modification before running melting-pot
+cache_modified_pre=0
+cache_modified_post=0
+
+if [ -f "~/.cache/scijava/melting-pot" ]; then
+  cache_modified_pre=$(find ~/.cache/scijava/melting-pot -printf '%T@\n' | sort -r | head -n 1)
+  cache_modified_pre=${cache_modified_pre%.*}
+fi
+
+# run melting-pot
+sh tests/run.sh
+
+# Record the last time of cache modification after running melting-pot
+if [ -f "~/.cache/scijava/melting-pot" ]; then
+  cache_modified_post=$(find ~/.cache/scijava/melting-pot -printf '%T@\n' | sort -r | head -n 1)
+  cache_modified_post=${cache_modified_post%.*}
+fi
+
+# Determine if cache needs to be re-generated
+if [ "$cache_modified_post" -gt "$cache_modified_pre" ]; then
+  echo "update-cache=true" >> $GITHUB_ENV
+else
+  echo "update-cache=false" >> $GITHUB_ENV
+fi
--- .github/setup.sh
+++ .github/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-setup-github-actions.sh
 sh ci-setup-github-actions.sh
+
+# xmllint needed for melting pot
+sudo apt-get install libxml2-utils
