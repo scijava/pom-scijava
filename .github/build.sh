b463aa355c7d537e59bcdb8f73387df8f89da20e
hinerm <hinerm@gmail.com>
2022-09-26 11:49:05 -0500

    build.sh: consolidate cache modification checks

--- .github/build.sh
+++ .github/build.sh
@@ -3,13 +3,19 @@ set -e
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-build.sh
 sh ci-build.sh
 
+# Helper method to get the last cache modified date as seconds since epoch
+last_cache_modified () {
+  cache_modified=$(find ~/.cache/scijava/melting-pot -printf '%T@\n' | sort -r | head -n1)
+  cache_modified=${cache_modified%.*}
+  echo "$cache_modified"
+}
+
 # Record the last time of cache modification before running melting-pot
 cache_modified_pre=0
 cache_modified_post=0
 
 if [ -f "~/.cache/scijava/melting-pot" ]; then
-  cache_modified_pre=$(find ~/.cache/scijava/melting-pot -printf '%T@\n' | sort -r | head -n 1)
-  cache_modified_pre=${cache_modified_pre%.*}
+  cache_modified_pre=$(last_cache_modified)
 fi
 
 # run melting-pot
@@ -17,8 +23,7 @@ tests/run.sh
 
 # Record the last time of cache modification after running melting-pot
 if [ -f "~/.cache/scijava/melting-pot" ]; then
-  cache_modified_post=$(find ~/.cache/scijava/melting-pot -printf '%T@\n' | sort -r | head -n 1)
-  cache_modified_post=${cache_modified_post%.*}
+  cache_modified_post=$(last_cache_modified)
 fi
 
 # Determine if cache needs to be re-generated
