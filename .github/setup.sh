164c60d8a9938d318e319c1c89821816fc4aa88b
Curtis Rueden <ctrueden@wisc.edu>
2022-12-15 09:32:28 -0600

    CI: call apt-get update before install
    Hoping to avoid errors like:
    
      Ign:8 http://azure.archive.ubuntu.com/ubuntu jammy-updates/main amd64 libxml2-utils amd64 2.9.13+dfsg-1ubuntu0.1
      Err:8 http://azure.archive.ubuntu.com/ubuntu jammy-updates/main amd64 libxml2-utils amd64 2.9.13+dfsg-1ubuntu0.1
        404  Not Found [IP: 52.252.75.106 80]
      Fetched 2359 kB in 0s (10.1 MB/s)
      E: Failed to fetch http://azure.archive.ubuntu.com/ubuntu/pool/main/libx/libxml2/libxml2-utils_2.9.13%2bdfsg-1ubuntu0.1_amd64.deb  404  Not Found [IP: 52.252.75.106 80]
      E: Unable to fetch some archives, maybe run apt-get update or try with --fix-missing?
      Error: Process completed with exit code 100.

--- .github/setup.sh
+++ .github/setup.sh
@@ -7,4 +7,5 @@ pkgs="libxml2-utils"       # needed for melting pot
 pkgs="$pkgs libxcb-shape0" # org.janelia:H5J_Loader_Plugin (fiji/H5J_Loader_Plugin@d026a1bb)
 pkgs="$pkgs libgtk2.0-0"   # net.imagej:imagej-opencv (imagej/imagej-opencv@21113e08)
 pkgs="$pkgs libblosc1"     # org.janelia.saalfeldlab:n5-blosc
+sudo apt-get update
 sudo apt-get -y install $(echo "$pkgs")
