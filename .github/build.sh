9c9b3d13f6bd8a999a5f32b1000add59381e40fe
Curtis Rueden <ctrueden@wisc.edu>
2023-05-23 09:57:23 -0500

    Fix the release workflow
    * Try harder to skip mega-melt when releasing.
    * Change release workflow name to "release".

--- .github/build.sh
+++ .github/build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
+# Discern whether this is a release build.
+releasing=
+if [ -f release.properties ]; then
+  releasing=1
+fi
+
+# Run the SciJava CI build script.
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-build.sh &&
 sh ci-build.sh || { echo "Maven build failed. Skipping melting pot tests."; exit 1; }
 
-if [ -f release.properties ]; then
-  # Skip melting pot when cutting a release.
+# Skip melting pot if cutting a release.
+if [ "$releasing" ]; then
   exit 0
 fi
 
