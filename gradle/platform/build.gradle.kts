397d5d0443ac3c00738d6428e90d7399b7d60f6e
Giuseppe Barbieri <elect86@gmail.com>
2024-03-05 15:30:42 +0100

    additional publishing info

--- gradle/platform/build.gradle.kts
+++ gradle/platform/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
             val a = node / "artifactId"
             val v = node / "version"
             api("$g:$a:$v")
-//            println("$g:$a:$v")
+            //            println("$g:$a:$v")
         }
     }
 }
@@ -45,6 +45,95 @@ publishing {
             version = (effXml / "version").toString()
 
             from(components["javaPlatform"])
+            pom {
+                name = "SciJava Parent POM"
+                description = "This POM provides a parent from which participating projects can declare their build configurations. It ensures that projects all use a compatible build environment, including Java version, as well as versions of dependencies and plugins."
+                url = "https://scijava.org/"
+                inceptionYear = "2011"
+                organization {
+                    name = "SciJava"
+                    url = "https://scijava.org/"
+                }
+                licenses {
+                    license {
+                        name = "Unlicense"
+                        url = "https://unlicense.org/"
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "ctrueden"
+                        name = "Curtis Rueden"
+                        url = "https://imagej.net/people/ctrueden"
+                        roles.addAll("founder", "lead", "developer", "debugger", "reviewer", "support", "maintainer")
+                    }
+                }
+                contributors {
+                    operator fun String.invoke(id: String) = contributor {
+                        name = this@invoke
+                        url = "https://imagej.net/people/$id"
+                        properties = mapOf("id" to id)
+                    }
+                    "Mark Hiner"("hinerm")
+                    "Johannes Schindelin"("dscho")
+                    "Sébastien Besson"("sbesson")
+                    "John Bogovic"("bogovicj")
+                    "Nicolas Chiaruttini"("NicoKiaru")
+                    "Barry DeZonia"("bdezonia")
+                    "Richard Domander"("rimadoma")
+                    "Karl Duderstadt"("karlduderstadt")
+                    "Jan Eglinger"("imagejan")
+                    "Gabriel Einsdorf"("gab1one")
+                    "Tiago Ferreira"("tferr")
+                    contributor {
+                        name = "David Gault"
+                        properties = mapOf("id" to "dgault")
+                    }
+                    "Ulrik Günther"("skalarproduktraum")
+                    "Philipp Hanslovsky"("hanslovsky")
+                    "Stefan Helfrich"("stelfrich")
+                    "Cameron Lloyd"("camlloyd")
+                    "Hadrien Mary"("hadim")
+                    "Tobias Pietzsch"("tpietzsch")
+                    "Stephan Preibisch"("StephanPreibisch")
+                    "Stephan Saalfeld"("axtimwalde")
+                    "Deborah Schmidt"("frauzufall")
+                    "Lorenzo Scianatico"("LoreScianatico")
+                    "Jean - Yves Tinevez"("tinevez")
+                    "Christian Tischer"("tischi")
+                    "Gabriella Turek"("turekg")
+                    contributor {
+                        name = "Giuseppe Barbieri"
+                        properties = mapOf("id" to "elect")
+                    }
+                }
+
+                mailingLists {
+                    mailingList {
+                        name = "SciJava"
+                        subscribe = "https://groups.google.com/group/scijava"
+                        unsubscribe = subscribe
+                        post = "scijava@googlegroups.com"
+                        archive = "https://groups.google.com/group/scijava"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:https://github.com/scijava/pom-scijava"
+                    developerConnection = "scm:git:git@github.com:scijava/pom-scijava"
+                    tag = "HEAD"
+                    url = "https://github.com/scijava/pom-scijava"
+                }
+                issueManagement {
+                    system = "GitHub Issues"
+                    url = "https://github.com/scijava/pom-scijava/issues"
+                }
+                ciManagement {
+                    system = "GitHub Actions"
+                    url = "https: //github.com/scijava/pom-scijava/actions"
+                }
+            }
         }
     }
 }
