56d08f25dd546098ba321eb7387ab711a55a6df3
Giuseppe Barbieri <elect86@gmail.com>
2024-03-12 09:18:55 +0100

    up

---
+++ gradle/platform/buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+//    gradlePluginPortal()
+}
+
+//dependencies {
+//    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+//}
