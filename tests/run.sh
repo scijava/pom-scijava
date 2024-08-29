86c3e237a426a040125fc5c413d3880433d49f18
Curtis Rueden <ctrueden@wisc.edu>
2024-08-29 11:19:21 -0500

    WIP: Update component versions for ImgLib2 v7
    * imagej-mesh: 0.8.1 -> 0.8.2
    * imagej-ops: 2.1.0 -> 2.2.0
    * imglib2-mesh: 1.0.0 -> 1.1.0
    * SPIM_Registration: 5.0.25 -> 5.0.26
    * TrackMate: 7.12.1 -> 7.14.0
    * TrackMate-Skeleton: 2.0.1 -> 2.0.3
    * labkit-pixel-classification: 0.1.17 -> 0.1.18
    * labkit-ui: 0.3.11 -> 0.4.0

--- tests/run.sh
+++ tests/run.sh
@@ -149,16 +149,8 @@ echo 'Hacking in any changed components...'
 # 5. Be added to the list of components to build in melt.sh (if not already present).
 components='
 org.janelia.saalfeldlab|n5-imglib2|git@github.com:saalfeldlab/n5-imglib2|pull/54/head
-sc.fiji|SPIM_Registration|git@github.com:fiji/SPIM_Registration|pull/142/head
-sc.fiji|labkit-ui|git@github.com:juglab/labkit-ui|pull/115/head
-sc.fiji|labkit-pixel-classification|git@github.com:juglab/labkit-pixel-classification|pull/12/head
 sc.fiji|z_spacing|git@github.com:saalfeldlab/z-spacing|pull/28/head
-sc.fiji|TrackMate|git@github.com:trackmate-sc/TrackMate|pull/289/head
-sc.fiji|TrackMate-Skeleton|git@github.com:trackmate-sc/TrackMate-Skeleton|pull/2/head
 sc.fiji|bigwarp_fiji|git@github.com:saalfeldlab/bigwarp|pull/170/head
-net.imagej|imagej-ops|git@github.com:imagej/imagej-ops|pull/654/head
-net.imagej|imagej-mesh|git@github.com:imagej/imagej-mesh|pull/27/head
-net.imglib2|imglib2-mesh|git@github.com:imglib/imglib2-mesh|pull/12/head
 '
 failFile="$meltingPotDir/fail"
 rm -f "$failFile"
