47bc3dcfabaf2d77cf4f749d2f853d3c51d80778
Curtis Rueden <ctrueden@wisc.edu>
2026-05-26 14:05:40 -0500

    CI: always use latest pombast (for now)

--- .github/status.sh
+++ .github/status.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-uv tool install "git+https://github.com/scijava/pombast.git@1cece3cab4d9be78fe06203db6f9b87d4db99f2f"
+uv tool install "git+https://github.com/scijava/pombast.git"
 pombast status .
 pombast team . --html team.html
 .github/publish.sh "Update status reports" index.html team.html
