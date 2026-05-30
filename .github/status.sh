71be2ef092b5ea3e7e3baa70c9e1055464802b91
Curtis Rueden <ctrueden@wisc.edu>
2026-05-30 12:58:03 -0500

    CI: generate badges.json as part of status refresh

--- .github/status.sh
+++ .github/status.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 uv tool install "git+https://github.com/scijava/pombast.git"
 pombast status .
-pombast team . --html team.html
-.github/publish.sh "Update status reports" index.html team.html
+pombast badges -o badges.json .
+pombast team .
+.github/publish.sh "Update status reports" index.html badges.json team.html
